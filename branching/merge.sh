05613779244594eefe6568f3e0239a2000d3523d
vasya436 <smilesins@mail.ru>
2026-08-03 01:25:56 +0400

    prepare for merge and rebase

---
+++ branching/merge.sh
@@ -0,0 +1,10 @@
+cat > merge.sh << 'EOF'
+#!/bin/bash
+# display command line options
+
+count=1
+for param in "$*"; do
+    echo "$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+done
+EOF
