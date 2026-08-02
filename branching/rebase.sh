13de516ab73512410653ec60c759f465956c3647
vasya436 <smilesins@mail.ru>
2026-08-03 02:08:40 +0400

    main: update rebase.sh with separator

---
+++ branching/rebase.sh
@@ -0,0 +1,12 @@
+cat > branching/rebase.sh << 'EOF'
+#!/bin/bash
+# display command line options
+
+count=1
+for param in "$@"; do
+    echo "$@ Parameter #$count = $param"
+    count=$(( $count + 1 ))
+done
+
+echo "====="
+EOF
