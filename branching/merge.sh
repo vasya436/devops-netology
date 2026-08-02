05a3bf029d25c938b356d267447be7e7984362c4
vasya436 <smilesins@mail.ru>
2026-08-03 01:31:46 +0400

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -3,8 +3,9 @@ cat > merge.sh << 'EOF'
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
 EOF
+
